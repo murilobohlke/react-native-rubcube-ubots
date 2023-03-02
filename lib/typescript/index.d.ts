5f4307e5c2917a16d5f714d4af9b15b90b2146c3
Murilo Bohlke <murilo.bohlke@rubcube.com>
2023-03-01 22:35:16 -0300

    feat: bob build

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,2 +1,5 @@
 export declare function multiply(a: number, b: number): Promise<number>;
+export declare function initialize(ubotsJID: string, hostname: string, domain: string, resource: string, port: number): void;
+export declare function openChat(botName: string): void;
+export declare function disconnect(): void;
 //# sourceMappingURL=index.d.ts.map
