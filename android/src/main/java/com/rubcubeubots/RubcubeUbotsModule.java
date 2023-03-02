e72db6335a54362a2362e7585569e39e26c4975b
Murilo Bohlke <murilo.bohlke@rubcube.com>
2023-03-01 22:28:34 -0300

    feat: android ubots

--- android/src/main/java/com/rubcubeubots/RubcubeUbotsModule.java
+++ android/src/main/java/com/rubcubeubots/RubcubeUbotsModule.java
@@ -8,6 +8,9 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
+import br.com.ubots.sdk.UBotsSDK;
+import br.com.ubots.sdk.presentation.ui.chat.ChatActivity;
+
 @ReactModule(name = RubcubeUbotsModule.NAME)
 public class RubcubeUbotsModule extends ReactContextBaseJavaModule {
   public static final String NAME = "RubcubeUbots";
@@ -28,5 +31,30 @@ public class RubcubeUbotsModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void multiply(double a, double b, Promise promise) {
     promise.resolve(a * b);
+  }
+
+  @ReactMethod
+  public void initialize(String ubotsJID, String hostname, String domain, String resource, int port) {
+    UBotsSDK.Companion.getInstance(getCurrentActivity()).initAnonymous(
+      ubotsJID,
+      hostname,
+      domain,
+      resource,
+      port
+    );
+
+    UBotsSDK.Companion.getInstance(getCurrentActivity()).connect();
+  }
+
+  @ReactMethod
+  public void openChat(String botName) {
+    // Intent i = new Intent(getCurrentActivity(), ChatActivity.class);
+    // i.putExtra("title", botName);
+    // getCurrentActivity().startActivity(i);
+  }
+
+  @ReactMethod
+  public void disconnect() {
+    
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -20,3 +20,21 @@ const RubcubeUbots = NativeModules.RubcubeUbots
 export function multiply(a: number, b: number): Promise<number> {
   return RubcubeUbots.multiply(a, b);
 }
+
+export function initialize(
+  ubotsJID: string,
+  hostname: string,
+  domain: string,
+  resource: string,
+  port: number
+): void {
+  return RubcubeUbots.openChat(ubotsJID, hostname, domain, resource, port);
+}
+
+export function openChat(botName: string): void {
+  return RubcubeUbots.openChat(botName);
+}
+
+export function disconnect(): void {
+  return RubcubeUbots.disconnect();
+}
